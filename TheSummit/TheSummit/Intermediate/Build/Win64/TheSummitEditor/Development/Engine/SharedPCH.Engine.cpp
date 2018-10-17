3f2c3e8b2b0637c525679b28f69d217113c6e985
earth418 <alihafez42@gmail.com>
2018-10-17 17:11:42 -0400

    a lot of changes; trying to fix
    hopefully this works;
    bismilla al-rahman al-rahim

---
+++ TheSummit/TheSummit/Intermediate/Build/Win64/TheSummitEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/Github/climb-summit/TheSummit/Intermediate/Build/Win64/TheSummitEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ TheSummit/TheSummit/Source/TheSummit/TheSummit.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheSummit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheSummit, "TheSummit" );
